0e057da3883ece0f516f73be9c5ba8b4e5065149
xPromate <jpcm_1998@hotmail.com>
2020-05-06 00:49:24 +0100

    changed messages and variables

--- scripts/emailServer.js
+++ scripts/emailServer.js
@@ -1,24 +1,27 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
+const email = "covidtrackingsystem@gmail.com";
+const pw = "joaodanieljoao20";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
   auth: {
-    user: 'covidtrackingsystem@gmail.com',
-    pass: 'joaodanieljoao20'
-  }
+    user: email,
+    pass: pw,
+  },
 });
 
-var mailOptions = {
-  from: 'covidtrackingsystem@gmail.com',
-  to: 'joaojon7@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+const mailOptions = {
+  from: email,
+  to: "joaojon7@gmail.com",
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
 };
 
-transporter.sendMail(mailOptions, function(error, info){
+transporter.sendMail(mailOptions, function (error, info) {
   if (error) {
     console.log(error);
   } else {
-    console.log('Email sent: ' + info.response);
+    console.log("Email sent: " + info.response);
   }
 });
