da7d1b29c73726bc4a608fe9a912817112d20532
Coimbra <joaojon7@gmail.com>
2020-05-09 18:57:05 +0100

    replaceOfTheMetaOnSchemas

--- api/models/CovidTest.js
+++ api/models/CovidTest.js
@@ -1,62 +1,52 @@
 // UUID Package
 const shortid = require('shortid');
 shortid.characters(
-	'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-@'
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-@'
 );
+
+const schemaOptions = {
+    timestamps: {
+        createdAt: 'createdAt',
+        updatedAt: 'updatedAt',
+    },
+};
 
 // Mongoose Package
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const {
+    Schema
+} = mongoose;
 
 // Set Schema
 const covidTestSchema = new Schema({
-	code: {
-		type: String,
-		unique: true,
-		default: '134a2i',
-	},
-	patient: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Patient',
-		required: true,
-	},
-	notes: {
-		type: String,
-	},
-	status: {
-		type: String,
-		enum: ['pending', 'inProgress', 'finished', 'notRealized'],
-		default: 'pending',
-	},
-	result: {
-		type: String,
-		enum: ['positive', 'negative', 'inconclusive'],
-	},
-	date: {
-		type: Date,
-		required: true,
-	},
-	meta: {
-		createdAt: {
-			type: Date,
-			default: Date.now(),
-		},
-		updatedAt: {
-			type: Date,
-		},
-	},
-});
-
-// Not use arrow function because to use "this""
-covidTestSchema.pre('save', function (next) {
-	if (this.isNew) {
-		this.code = shortid.generate();
-		this.meta.createdAt = this.meta.updatedAt = Date.now();
-	} else {
-		this.meta.updatedAt = Date.now();
-	}
-
-	next();
-});
+        code: {
+            type: String,
+            unique: true,
+            default: '134a2i',
+        },
+        patient: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Patient',
+            required: true,
+        },
+        notes: {
+            type: String,
+        },
+        status: {
+            type: String,
+            enum: ['pending', 'inProgress', 'finished', 'notRealized'],
+            default: 'pending',
+        },
+        result: {
+            type: String,
+            enum: ['positive', 'negative', 'inconclusive'],
+        },
+        date: {
+            type: Date,
+            required: true,
+        },
+    },
+    schemaOptions,
+);
 
 module.exports = mongoose.model('CovidTest', covidTestSchema);
--- api/models/Patient.js
+++ api/models/Patient.js
@@ -1,90 +1,80 @@
 // Mongoose Package
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const {
+    Schema
+} = mongoose;
+
+const schemaOptions = {
+    timestamps: {
+        createdAt: 'createdAt',
+        updatedAt: 'updatedAt',
+    },
+};
 
 // Set Schema
 const patientSchema = new Schema({
-	name: {
-		type: String,
-		require: true,
-	},
-	birthdayDate: {
-		type: Date,
-		require: true,
-	},
-	patientNumber: {
-		// N.º de Utente
-		type: Number,
-		require: true,
-		unique: true,
-	},
-	status: {
-		type: String,
-		enum: ['Suspect', 'Infected', 'Non Infected'],
-		default: 'Suspect',
-	},
-	contacts: {
-		phone: {
-			type: Number,
-			minlength: 9,
-			maxlength: 9,
-			validate: {
-				validator: function (data) {
-					return /^\d{9}$/.test(data);
-				},
-				message: (props) =>
-					`${props.value} is not a valid contact phone`,
-			},
-			require: [true, 'User contact require'],
-			unique: true,
-		},
-		email: {
-			type: String,
-			validate: {
-				validator: function (data) {
-					return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(data);
-				},
-				message: (props) => `${props.value} is not a valid email!`,
-			},
-			require: [true, 'User email require'],
-			unique: true,
-		},
-	},
-	symptoms: [
-		{
-			type: String,
-			enum: [
-				'cough',
-				'fever',
-				'shortness of breathe',
-				'lack of smell',
-				'lack of taste',
-				'tiredness',
-				'burning eyes',
-				'headaches',
-				'diarrhea',
-			],
-		},
-	],
-	meta: {
-		createdAt: {
-			type: Date,
-			default: Date.now(),
-		},
-		updatedAt: {
-			type: Date,
-		},
-	},
-});
-
-patientSchema.pre('save', function (next) {
-	if (this.isNew) {
-		this.meta.createdAt = this.meta.updatedAt = Date.now();
-	} else {
-		this.meta.updatedAt = Date.now();
-	}
-
-	next();
-});
+        name: {
+            type: String,
+            require: true,
+        },
+        birthdayDate: {
+            type: Date,
+            require: true,
+        },
+        patientNumber: {
+            // N.º de Utente
+            type: Number,
+            require: true,
+            unique: true,
+        },
+        status: {
+            type: String,
+            enum: ['Suspect', 'Infected', 'Non Infected'],
+            default: 'Suspect',
+        },
+        contacts: {
+            phone: {
+                type: Number,
+                minlength: 9,
+                maxlength: 9,
+                validate: {
+                    validator: function (data) {
+                        return /^\d{9}$/.test(data);
+                    },
+                    message: (props) =>
+                        `${props.value} is not a valid contact phone`,
+                },
+                require: [true, 'Patient contact require'],
+                unique: true,
+            },
+            email: {
+                type: String,
+                validate: {
+                    validator: function (data) {
+                        return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(data);
+                    },
+                    message: (props) => `${props.value} is not a valid email!`,
+                },
+                require: [true, 'Patient email require'],
+                unique: true,
+            },
+        },
+        symptoms: [{
+            type: String,
+            enum: [
+                'cough',
+                'fever',
+                'shortness of breathe',
+                'lack of smell',
+                'lack of taste',
+                'tiredness',
+                'burning eyes',
+                'headaches',
+                'diarrhea',
+            ],
+        }, ],
+    },
+    schemaOptions,
+    );
 
 module.exports = mongoose.model('Patient', patientSchema);
