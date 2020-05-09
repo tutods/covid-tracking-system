4f03eb5b30f312800d4d11d2a6d67cafd989e55c
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-05-09 17:55:21 +0100

    Add Login Style

--- webapp/src/app/app-material/app-material.module.ts
+++ webapp/src/app/app-material/app-material.module.ts
@@ -2,12 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 const Components = [
@@ -19,7 +22,10 @@ const Components = [
 	MatCardModule,
 	MatMenuModule,
 	MatGridListModule,
-	MatProgressSpinnerModule
+	MatProgressSpinnerModule,
+	MatTabsModule,
+	MatFormFieldModule,
+	MatCheckboxModule
 ];
 
 
