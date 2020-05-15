14477caec2426c337b5cd91987983d259e6c1bd8
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-05-15 14:40:50 +0100

    Insert default user

--- scripts/defaultRoles.js
+++ scripts/defaultRoles.js
@@ -1,6 +1,7 @@
 // Mongoose Connection
 const mongoose = require('../config/mongoose');
 const Role = require('../api/models/Role');
+const User = require('../api/models/User');
 
 mongoose
 	.then(async (mongoose) => {
@@ -9,7 +10,22 @@ mongoose
 		try {
 			const result = await Role.insertMany(rolesList);
 
-			console.log('[ROLES INSERTED WITH SUCCESS]');
+			if (result) {
+				const admin = await Role.findOne({ name: 'ADMIN' });
+
+				const user = await new User({
+					name: 'COVID Test',
+					email: 'covidtrackingsystem@gmail.com',
+					password: 'joaodanieljoao20',
+					role: admin._id,
+				}).save();
+
+				if (user) {
+					console.log('[ROLES AND USER INSERTED WITH SUCCESS]');
+				}
+			} else {
+				console.log('[ROLES NOT INSERTED]');
+			}
 		} catch (error) {
 			console.error(error);
 		}
