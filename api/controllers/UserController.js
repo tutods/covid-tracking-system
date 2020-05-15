1b7da900038320bf3b0e696ba251b57217123646
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-05-15 16:13:05 +0100

    Reset Password email - ✓

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,166 +1,169 @@
 // ENV
-require("dotenv").config();
-const { SECRET = "coV!d#19_$ystem$", EXPIRES = 1800000 } = process.env;
+require('dotenv').config();
+const { SECRET = 'coV!d#19_$ystem$', EXPIRES = 1800000 } = process.env;
 
 // Model
-const User = require("../models/User");
+const User = require('../models/User');
 
 // Packages
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 //Nodemailer
-const nodemailer = require("nodemailer");
-const ejs = require("ejs");
-const fs = require("fs");
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const fs = require('fs');
 
 const userController = () => {
-  const login = async (req, res) => {
-    const data = req.body;
-    const bodyEmail = data.email;
-    const bodyPwd = data.password;
+	const login = async (req, res) => {
+		const data = req.body;
+		const bodyEmail = data.email;
+		const bodyPwd = data.password;
 
-    const userDB = await User.findOne({ email: bodyEmail });
+		const userDB = await User.findOne({ email: bodyEmail });
 
-    if (userDB) {
-      let response = {};
+		if (userDB) {
+			let response = {};
 
-      const validatePwd = await userDB.comparePassword(bodyPwd);
+			const validatePwd = await userDB.comparePassword(bodyPwd);
 
-      if (validatePwd) {
-        const user = {
-          email: userDB.email,
-          name: userDB.name,
-          role: userDB.role.name,
-          scopes: userDB.role.scopes,
-        };
-        const jwtT = jwt.sign(user, SECRET);
+			if (validatePwd) {
+				const user = {
+					email: userDB.email,
+					name: userDB.name,
+					role: userDB.role.name,
+					scopes: userDB.role.scopes,
+				};
+				const jwtT = jwt.sign(user, SECRET);
 
-        res.cookie("session", jwtT, {
-          expires: new Date(Date.now() + EXPIRES),
-        });
+				res.cookie('session', jwtT, {
+					expires: new Date(Date.now() + EXPIRES),
+				});
 
-        response.code = 200;
-        response.auth = true;
-        response.token = jwtT;
-        response.message = "";
-        response.user = user;
-      } else {
-        response.code = 401;
-        response.auth = false;
-        response.token = null;
-        response.message = "Invalid Credentials";
-      }
+				response.code = 200;
+				response.auth = true;
+				response.token = jwtT;
+				response.message = '';
+				response.user = user;
+			} else {
+				response.code = 401;
+				response.auth = false;
+				response.token = null;
+				response.message = 'Invalid Credentials';
+			}
 
-      res.status(response.code).json({
-        auth: response.auth,
-        token: response.token,
-        message: response.message,
-        user: response.user || {},
-      });
-    } else {
-      res.status(401).json({
-        auth: false,
-        token: null,
-        message: "Invalid Credentials",
-        user: {},
-      });
-    }
-  };
+			res.status(response.code).json({
+				auth: response.auth,
+				token: response.token,
+				message: response.message,
+				user: response.user || {},
+			});
+		} else {
+			res.status(401).json({
+				auth: false,
+				token: null,
+				message: 'Invalid Credentials',
+				user: {},
+			});
+		}
+	};
 
-  const resetPassword = async (req, res) => {
-    const email = req.body.email;
+	const resetPassword = async (req, res) => {
+		const email = req.body.email;
 
-    const user = await User.findOne({ email: email });
+		const user = await User.findOne({ email: email });
 
-    if (user == null) {
-      res.status(401).json({
-        auth: false,
-        token: null,
-        message: "Invalid Credentials",
-      });
-    } else {
-      const jwtT = jwt.sign(user.email, SECRET);
+		if (user == null) {
+			res.status(401).json({
+				auth: false,
+				token: null,
+				message: 'Invalid Credentials',
+			});
+		} else {
+			const jwtT = jwt.sign(user.email, SECRET);
 
-      res.cookie("reset", jwtT, {
-        expires: new Date(Date.now + 300000),
-      });
+			res.cookie('reset', jwtT, {
+				expires: new Date(Date.now + 300000),
+			});
 
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: "covidtrackingsystem@gmail.com",
-          pass: "joaodanieljoao20",
-        },
-      });
+			const transporter = nodemailer.createTransport({
+				service: 'gmail',
+				auth: {
+					user: 'covidtrackingsystem@gmail.com',
+					pass: 'joaodanieljoao20',
+				},
+			});
 
-      ejs.renderFile("./views/mail/reset.ejs", { link: "http://localhost:3000/api/users/changePassword/" + jwtT }, function (
-        err,
-        data
-      ) {
-        if (err) {
-          console.log(err);
-        } else {
-          var mainOptions = {
-            from: "covidtrackingsystem@gmail.com",
-            to: email,
-            subject: "Covid-19",
-            html: data,
-          };
-          console.log("html data ======================>", mainOptions.html);
-          transporter.sendMail(mainOptions, function (err, info) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log("Message sent: " + info.response);
-            }
-          });
-        }
-      });
-    }
-  };
+			ejs.renderFile(
+				'./views/mail/reset.ejs',
+				{
+					link:
+						'http://localhost:3000/api/users/changePassword/' +
+						jwtT,
+				},
+				function (err, data) {
+					if (err) {
+						console.log(err);
+					} else {
+						var mainOptions = {
+							from: 'covidtrackingsystem@gmail.com',
+							to: email,
+							subject: 'Covid-19',
+							html: data,
+						};
 
-  const changePassword = async (req, res) => {
-    const token = req.params.token;
-    const data = req.body;
+						transporter.sendMail(mainOptions, function (err, info) {
+							if (err) {
+								console.log(err);
+							} else {
+								console.log('Message sent: ' + info.response);
+							}
+						});
+					}
+				}
+			);
+		}
+	};
 
-    const cookie = req.cookies.reset;
+	const changePassword = async (req, res) => {
+		const token = req.params.token;
+		const data = req.body;
 
-    if (jwt.verify(token, SECRET) && cookie) {
-      const newPassword = data.newPassword;
-      const confirmPassword = data.confirmPassword;
+		const cookie = req.cookies.reset;
 
-		if (newPassword !== confirmPassword) {
+		if (jwt.verify(token, SECRET) && cookie) {
+			const newPassword = data.newPassword;
+			const confirmPassword = data.confirmPassword;
 
-        res.status(401).json({
-          message: "Passwords d'ont match",
-		});
+			if (newPassword !== confirmPassword) {
+				res.status(401).json({
+					message: "Passwords d'ont match",
+				});
+			} else {
+				const email = jwt.verify(token, SECRET);
 
-      } else {
-        const email = jwt.verify(token, SECRET);
+				const user = await User.findOneAndUpdate(
+					{ email: email },
+					{ password: newPassword }
+				);
 
-        const user = await User.findOneAndUpdate(
-          { email: email },
-          {password: newPassword,}
-		);
+				res.status(200).json({
+					user: user,
+					message: 'Password reset sucessfully !',
+				});
+			}
+		} else {
+			res.status(408).json({
+				message: 'Token expire',
+			});
+		}
+	};
 
-        res.status(200).json({
-          user: user,
-          message: "Password reset sucessfully !",
-        });
-      }
-    }else{
-		res.status(408).json({
-			message: "Token expire"
-		})
-	}
-  };
+	const logout = (req, res) => {
+		// Destroy cookie
+		res.clearCookie('session').json({ success: true });
+	};
 
-  const logout = (req, res) => {
-    // Destroy cookie
-    res.clearCookie("session").json({ success: true });
-  };
-
-  return { login, logout, resetPassword, changePassword };
+	return { login, logout, resetPassword, changePassword };
 };
 
 module.exports = userController();
