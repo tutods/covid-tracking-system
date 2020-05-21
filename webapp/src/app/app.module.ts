9199ff3ed802d04d090e65fcb4a6d163a4b7a712
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-05-21 19:10:10 +0100

    Angular proxy and fix login duplicated request

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,6 +2,14 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Material Module Imports
@@ -33,6 +41,14 @@ import { PatientsComponent } from './pages/patients/patients.component';
 		BrowserAnimationsModule,
 		ReactiveFormsModule,
 		LayoutModule,
+		MatGridListModule,
+		MatCardModule,
+		MatMenuModule,
+		MatIconModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatListModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
