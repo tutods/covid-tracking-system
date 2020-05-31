25cf060655a5f8de7bc7d0039600f44fb4644dd3
Coimbra <joaojon7@gmail.com>
2020-05-31 19:56:05 +0100

    covidTestServiceImplemented

---
+++ frontend/src/app/pages/covid-test/covid-test/covid-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-covid-test',
+  templateUrl: './covid-test.component.html',
+  styleUrls: ['./covid-test.component.sass']
+})
+export class CovidTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
