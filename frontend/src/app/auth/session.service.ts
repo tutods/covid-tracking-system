1e4e86d681009ad981ca0a2c8ba71f2f9b942a78
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-06-01 10:19:07 +0100

    MERGE

--- frontend/src/app/auth/session.service.ts
+++ frontend/src/app/auth/session.service.ts
@@ -1,14 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-import { share } from "rxjs/operators";
-import { environment } from "../../environments/environment";
-import { Observable } from 'rxjs';
-=======
 import { Observable } from 'rxjs';
 import { share } from "rxjs/operators";
 import { environment } from "../../environments/environment";
->>>>>>> development
 
 const API_URL = environment.apiUrl;
 
