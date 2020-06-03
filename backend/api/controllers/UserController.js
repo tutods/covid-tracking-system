d3b1970b9e9035769e4c55573685c59ee5eccab2
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-06-03 15:20:44 +0100

    Add catch error on login

--- backend/api/controllers/UserController.js
+++ backend/api/controllers/UserController.js
@@ -12,57 +12,63 @@ const newUserEmail = require('../../scripts/emails/newUserEmail');
 const jwt = require('jsonwebtoken');
 
 const userController = () => {
-	const login = async (req, res) => {
+	const login = async (req, res, next) => {
 		const data = req.body;
 		const bodyEmail = data.email;
 		const bodyPwd = data.password;
 
-		const userDB = await User.findOne({ email: bodyEmail });
+		try {
+			const userDB = await User.findOne({ email: bodyEmail });
 
-		if (userDB) {
-			let response = {};
+			if (userDB) {
+				let response = {};
 
-			const validatePwd = await userDB.comparePassword(bodyPwd);
+				const validatePwd = userDB.comparePassword(bodyPwd);
+				if (validatePwd) {
+					const user = {
+						email: userDB.email,
+						name: userDB.name,
+						role: userDB.role.name,
+						scopes: userDB.role.scopes,
+					};
+					const jwtT = await jwt.sign(user, SECRET, {
+						expiresIn: `${EXPIRES}ms`,
+					});
 
-			if (validatePwd) {
-				const user = {
-					email: userDB.email,
-					name: userDB.name,
-					role: userDB.role.name,
-					scopes: userDB.role.scopes,
-				};
-				const jwtT = await jwt.sign(user, SECRET, {
-					expiresIn: `${EXPIRES}ms`,
+					res.cookie('session', jwtT, {
+						expires: new Date(Date.now() + EXPIRES),
+					});
+
+					response.code = 200;
+					response.auth = true;
+					response.token = jwtT;
+					response.message = '';
+					response.user = user;
+				} else {
+					response.code = 401;
+					response.auth = false;
+					response.token = null;
+					response.message = 'Invalid Credentials';
+				}
+
+				res.status(response.code).json({
+					auth: response.auth,
+					token: response.token,
+					message: response.message,
+					user: response.user || {},
 				});
-
-				res.cookie('session', jwtT, {
-					expires: new Date(Date.now() + EXPIRES),
-				});
-
-				response.code = 200;
-				response.auth = true;
-				response.token = jwtT;
-				response.message = '';
-				response.user = user;
 			} else {
-				response.code = 401;
-				response.auth = false;
-				response.token = null;
-				response.message = 'Invalid Credentials';
+				res.status(401).json({
+					auth: false,
+					token: null,
+					message: 'Invalid Credentials',
+					user: {},
+				});
 			}
-
-			res.status(response.code).json({
-				auth: response.auth,
-				token: response.token,
-				message: response.message,
-				user: response.user || {},
-			});
-		} else {
-			res.status(401).json({
-				auth: false,
-				token: null,
-				message: 'Invalid Credentials',
-				user: {},
+		} catch (catchError) {
+			next({
+				message: catchError,
+				status: 500,
 			});
 		}
 	};
