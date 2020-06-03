afdd64e950eabef2d4d7a0968ecd92fee84bf144
Daniel Sousa <jdaniel.asousa@gmail.com>
2020-06-03 16:03:04 +0100

    auth layout changes

--- frontend/src/app/layout/auth/auth.component.ts
+++ frontend/src/app/layout/auth/auth.component.ts
@@ -7,17 +7,8 @@ import { Router } from '@angular/router';
 	styleUrls: ['./auth.component.sass'],
 })
 export class AuthComponent implements OnInit {
-	title: string;
 
 	constructor(private router: Router) {
-		// Set Title
-		this.title = (this.router.url.includes('login'))
-			? "Sign In"
-			: (this.router.url.includes('reset-password'))
-				? "Reset Password"
-				: (this.router.url.includes('change-password'))
-					? "Change Password"
-					: "Welcome!"
 	}
 
 	ngOnInit(): void { }
