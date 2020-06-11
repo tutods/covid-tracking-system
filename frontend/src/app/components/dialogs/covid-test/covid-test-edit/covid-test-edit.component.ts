0cad7751b191367194bbb741355eb51eb95a3b98
Coimbra <joaojon7@gmail.com>
2020-06-11 13:58:11 +0100

    resultWithStatus

--- frontend/src/app/components/dialogs/covid-test/covid-test-edit/covid-test-edit.component.ts
+++ frontend/src/app/components/dialogs/covid-test/covid-test-edit/covid-test-edit.component.ts
@@ -5,84 +5,86 @@ import { CovidTest } from '../../../../models/covid-test.model';
 import { CovidTestService } from '../../../../services/covid-test/covid-test.service';
 
 @Component({
-	selector: 'app-covid-test-edit',
-	templateUrl: './covid-test-edit.component.html',
-	styleUrls: ['./covid-test-edit.component.sass']
+    selector: 'app-covid-test-edit',
+    templateUrl: './covid-test-edit.component.html',
+    styleUrls: ['./covid-test-edit.component.sass']
 })
 export class CovidTestEditComponent implements OnInit {
-	covidTest: CovidTest;
-	covidTestForm: FormGroup;
-	file: File = null
-	// Default Status
-	status: string[] = ['pending', 'in Progress', 'finished', 'waiting Result'];
-	results: string[] = ['positive', 'negative', 'inconclusive'];
+    covidTest: CovidTest;
+    covidTestForm: FormGroup;
+    file: File = null
+    // Default Status
+    status: string[] = ['pending', 'in Progress', 'finished', 'waiting Result'];
+    results: string[] = ['positive', 'negative', 'inconclusive'];
 
 
-	constructor(private formBuilder: FormBuilder, public covidTests: CovidTestService,
-		public dialogRef: MatDialogRef<CovidTestEditComponent>,
-		@Inject(MAT_DIALOG_DATA) data) {
-		this.covidTest = data;
-	}
+    constructor(private formBuilder: FormBuilder, public covidTests: CovidTestService,
+        public dialogRef: MatDialogRef<CovidTestEditComponent>,
+        @Inject(MAT_DIALOG_DATA) data) {
+        this.covidTest = data;
+    }
 
-	ngOnInit(): void {
-		this.covidTestForm = this.formBuilder.group({
-			'notes': [this.covidTest.notes, []],
-			'scheduleDate': [new Date(this.covidTest.date), [
-				Validators.required,
-			]],
-			'status': [this.covidTest.status, [
-				Validators.required,
-			]],
-			'result': [this.covidTest.result, []],
-			'file': ['', []]
-		});
-	}
+    ngOnInit(): void {
+        this.covidTestForm = this.formBuilder.group({
+            'notes': [this.covidTest.notes, []],
+            'scheduleDate': [new Date(this.covidTest.date), [
+                Validators.required,
+            ]],
+            'status': [this.covidTest.status, [
+                Validators.required,
+            ]],
+            'result': [this.covidTest.result, []],
+            'file': ['', []]
+        });
+    }
 
-	onFileSelected(e) {
-		this.file = <File>(e.target.files || [])[0]
-	}
+    onFileSelected(e) {
+        this.file = <File>(e.target.files || [])[0]
+    }
 
+    statusValue() {
+        return this.covidTestForm.get('status').value || '';
+    }
 
+    save(evt) {
+        evt.preventDefault()
 
-	save(evt) {
-		evt.preventDefault()
+        let formDate: Date = new Date(this.covidTestForm.get('scheduleDate').value)
+        formDate = new Date(`${formDate.getFullYear()}-${formDate.getMonth() + 1}-${formDate.getDate()}`)
 
-		let formDate: Date = new Date(this.covidTestForm.get('scheduleDate').value)
-		formDate = new Date(`${formDate.getFullYear()}-${formDate.getMonth() + 1}-${formDate.getDate()}`)
+        let fData = new FormData();
+        fData.append('notes', this.covidTestForm.get('notes').value)
+        fData.append('date', formDate.toString())
+        fData.append('status', this.covidTestForm.get('status').value)
 
-		let fData = new FormData();
-		fData.append('notes', this.covidTestForm.get('notes').value)
-		fData.append('date', formDate.toString())
-		fData.append('status', this.covidTestForm.get('status').value)
+        if (this.covidTestForm.get('result').value)
+            fData.append('result', this.covidTestForm.get('result').value)
 
-		if (this.covidTestForm.get('result').value)
-			fData.append('result', this.covidTestForm.get('result').value)
+        if (this.file)
+            fData.append('file', this.file, this.file.name)
 
-		if (this.file)
-			fData.append('file', this.file, this.file.name)
+        this.covidTests.getOneAndUpdate(this.covidTest._id, fData).subscribe(
+            (success) => {
+                this.dialogRef.close({
+                    message: "COVID Test updated with success!",
+                    status: true
+                })
+            }, (error) => {
+                const codeMessage = error.error.message;
+                this.dialogRef.close({
+                    message: codeMessage || "Error on update COVID Test. Try again please.",
+                    status: false
+                })
+            }
+        );
+    }
 
-		this.covidTests.getOneAndUpdate(this.covidTest._id, fData).subscribe(
-			(success) => {
-				this.dialogRef.close({
-					message: "COVID Test updated with success!",
-					status: true
-				})
-			}, (error) => {
-				const codeMessage = error.error.message;
-				this.dialogRef.close({
-					message: codeMessage || "Error on update COVID Test. Try again please.",
-					status: false
-				})
-			}
-		);
-	}
+    // To disable button if have errors
+    get covidTestFormControl() {
+        return this.covidTestForm.controls;
+    }
 
-	// To disable button if have errors
-	get covidTestFormControl() {
-		return this.covidTestForm.controls;
-	}
-
-	onClose(): void {
-		this.dialogRef.close();
-	}
+    onClose(): void {
+        this.dialogRef.close();
+    }
 }
